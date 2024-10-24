8f04086d74a8dc0853778ec158f7bc122f48b48e
Ronan Dubuisson <ronan.dubuisson@dubuissonconsulting.be>
2024-10-24 17:08:17 +0200

    added checkbox and hardcoded time to the note component

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -1,14 +1,28 @@
 import { Note as TNote } from "../types";
+import { useState } from "react";
 
 interface Props {
   note: TNote;
 }
 
 function Note({ note }: Props) {
+  const [isDone, setIsDone] = useState(false);
+
+  function setNoteClassNames() {
+    return isDone ? "note note-done" : "note";
+  }
+
   return (
-    <div className="note">
+    <div
+      className={setNoteClassNames()}
+      onClick={() => {
+        setIsDone(!isDone);
+      }}
+    >
       <h1>{note.id + " - " + note.title}</h1>
       <p>{note.body}</p>
+      <input type="checkbox" checked={isDone} readOnly />
+      <p className="time">24/10/2024 16:59</p>
     </div>
   );
 }
